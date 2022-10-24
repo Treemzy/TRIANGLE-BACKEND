27f679c74d12dfff8ccd486d2756ea78492493b9
Treemzy <Treemzy@gmail.com>
2022-10-24 15:07:47 +0100

    change

--- backend/settings.py
+++ backend/settings.py
@@ -14,7 +14,7 @@ import dj_database_url
 import django_heroku
 from pathlib import Path
 import os
-import dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,11 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-q-*2-82la&#k7dg3_zhtsn%t#mr$w#rmj3db6sjc+8x=nc4y^@'
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
